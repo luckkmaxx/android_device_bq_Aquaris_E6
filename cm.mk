80894047aab2ef735937ad9bff4c13d11c8c5acf
ElTifo <tifo.el@gmail.com>
2016-04-29 16:28:52 +0100

    Rename to Aquaris_E5_FHD

--- cm.mk
+++ cm.mk
@@ -1,17 +1,17 @@
 ## Specify phone tech before including full_phone
 
 # Release name
-PRODUCT_RELEASE_NAME := K1_turbo
+PRODUCT_RELEASE_NAME := Aquaris_E5_FHD
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/kingzone/k1_turbo/device_k1-turbo.mk)
+$(call inherit-product, device/bq/Aquaris_E5_FHD/device_Aquaris_E5_FHD.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := k1_turbo
-PRODUCT_NAME := cm_k1_turbo
-PRODUCT_BRAND := KINGZONE
-PRODUCT_MODEL := K1_turbo
-PRODUCT_MANUFACTURER := KINGZONE
+PRODUCT_DEVICE := Aquaris_E5_FHD
+PRODUCT_NAME := cm_Aquaris_E5_FHD
+PRODUCT_BRAND := bq
+PRODUCT_MODEL := Aquaris_E5_FHD
+PRODUCT_MANUFACTURER := bq
