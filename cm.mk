60986051b64cd23355de2c1ec760e2f4db3dbc4f
luckkmaxx <luckkmaxx@gmail.com>
2016-05-02 20:50:58 +0200

    Rename to Aquaris_E6

--- cm.mk
+++ cm.mk
@@ -1,17 +1,17 @@
 ## Specify phone tech before including full_phone
 
 # Release name
-PRODUCT_RELEASE_NAME := Aquaris_E5_FHD
+PRODUCT_RELEASE_NAME := Aquaris_E6
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/bq/Aquaris_E5_FHD/device_Aquaris_E5_FHD.mk)
+$(call inherit-product, device/bq/Aquaris_E6/device_Aquaris_E6.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := Aquaris_E5_FHD
-PRODUCT_NAME := cm_Aquaris_E5_FHD
+PRODUCT_DEVICE := Aquaris_E6
+PRODUCT_NAME := cm_Aquaris_E6
 PRODUCT_BRAND := bq
-PRODUCT_MODEL := Aquaris_E5_FHD
+PRODUCT_MODEL := Aquaris_E6
 PRODUCT_MANUFACTURER := bq
